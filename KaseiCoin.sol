65ea00fe98e3647353065596696034ff322b6680
Kyle Huber <kyhuber@gmail.com>
2022-05-27 10:34:06 -0700

    Update KaseiCoin.sol

--- KaseiCoin.sol
+++ KaseiCoin.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.5.0;
 
-//  Import contracts from the OpenZeppelin library:
+//  Import contracts from the OpenZeppelin library.
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-// Create a constructor for the KaseiCoin contract and have the contract inherit the libraries that you imported from OpenZeppelin.
+// Create a constructor for the contract and inherit the libraries from OpenZeppelin.
 
 contract KaseiCoin is ERC20, ERC20Detailed, ERC20Mintable {
     
